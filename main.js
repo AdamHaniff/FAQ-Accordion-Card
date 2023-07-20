3682a37470c939b73fb9d4baae2a39ac00ac475e
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-07-19 22:16:45 -0400

    Added logic to reveal answer

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+// VARIABLES
+const faqContainer = document.querySelector(".FAQ");
+
+// WHEN THE QUESTION CONTAINER IS CLICKED,
+// REMOVE THE HIDDEN CLASS FROM THE ANSWER;
+// MAKE THE QUESTION BOLD
+// ROTATE THE ARROW ICON
+
+// EVENT LISTENER
+faqContainer.addEventListener("click", function (e) {
+  const question = e.target.closest(".FAQ__QnA-container");
+  if (!question) return;
+  const answer = question.querySelector(".FAQ__answer");
+  answer.classList.toggle("hidden");
+});
